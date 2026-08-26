ac0026dce5a2bcca216b30174f3b4178aed832e0
waniakhan22 <YOUR_GITHUB_EMAIL>
2026-08-26 22:36:17 +0500

    Fix auth CSS import case for Vercel

--- Clothing Website/src/components/userLogin.jsx
+++ Clothing Website/src/components/userLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Auth.css";
+import "./auth.css";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
--- Clothing Website/src/components/userSignUp.jsx
+++ Clothing Website/src/components/userSignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Auth.css";
+import "./auth.css";
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
